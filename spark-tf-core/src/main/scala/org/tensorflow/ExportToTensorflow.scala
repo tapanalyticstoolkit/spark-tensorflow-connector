f94e51f3db63a009882df8385fc5634bfa8dd52b
Karthik Vadla <karthik.vadla@intel.com>
2017-01-25 11:38:36 -0800

    initial commit for spark-tensorflow-connector

---
+++ spark-tf-core/src/main/scala/org/tensorflow/ExportToTensorflow.scala
@@ -0,0 +1,18 @@
+package org.tensorflow
+
+import org.apache.hadoop.io.{ BytesWritable, NullWritable }
+import org.apache.spark.sql.DataFrame
+import org.tensorflow.hadoop.io.TFRecordFileOutputFormat
+import org.tensorflow.serde.DefaultTfRecordRowEncoder
+
+object ExportToTensorflow {
+
+  def exportToTensorflow(sourceFrame: DataFrame, destinationPath: String): Unit = {
+    val features = sourceFrame.map(row => {
+      val example = DefaultTfRecordRowEncoder.encodeTfRecord(row)
+      (new BytesWritable(example.toByteArray), NullWritable.get())
+    })
+    features.saveAsNewAPIHadoopFile[TFRecordFileOutputFormat](destinationPath)
+  }
+
+}
