ba0a8b626988c39d8968013d24f7723b615f2d28
Karthik Vadla <karthik.vadla@intel.com>
2017-02-08 15:02:01 -0800

    Remove import and export files and clean up

--- spark-tf-core/src/main/scala/org/tensorflow/DataTypesConvertor.scala
+++ spark-tf-core/src/main/scala/org/tensorflow/DataTypesConvertor.scala
@@ -15,72 +15,12 @@
  */
 package org.tensorflow
 
-import org.slf4j.LoggerFactory
 import scala.language.implicitConversions
 
 /**
  * DataTypes supported
  */
 object DataTypesConvertor {
-
-  lazy val logger = LoggerFactory.getLogger("DataTypesConvertor")
-  /**
-   * Attempt to cast Any type to Double
-   *
-   * @param value input Any type to be cast
-   * @return value cast as Double, if possible
-   */
-  def toDouble(value: Any): Double = {
-    value match {
-      case null => throw new IllegalArgumentException("null cannot be converted to Double")
-      case i: Int => i.toDouble
-      case l: Long => l.toDouble
-      case f: Float => f.toDouble
-      case d: Double => d
-      case bd: BigDecimal => bd.toDouble
-      case s: String => s.trim().toDouble
-      case _ => throw new IllegalArgumentException(s"The following value is not a numeric data type: $value")
-    }
-  }
-
-  def toBigDecimal(value: Any): BigDecimal = {
-    value match {
-      case null => null
-      case i: Int => BigDecimal(i)
-      case l: Long => BigDecimal(l)
-      case f: Float => BigDecimal(f)
-      case d: Double => BigDecimal(d)
-      case bd: BigDecimal => bd
-      case s: String => BigDecimal(s)
-      case _ => throw new IllegalArgumentException(s"The following value is not of numeric data type: $value")
-    }
-  }
-
-  def toLong(value: Any): Long = {
-    value match {
-      case null => throw new IllegalArgumentException("null cannot be converted to Long")
-      case i: Int => i.toLong
-      case l: Long => l
-      case f: Float => f.toLong
-      case d: Double => d.toLong
-      case bd: BigDecimal => bd.toLong
-      case s: String => s.trim().toLong
-      case _ => throw new RuntimeException(s"${value.getClass.getName} toLong is not implemented")
-    }
-  }
-
-  def toInt(value: Any): Int = {
-    value match {
-      case null => throw new IllegalArgumentException("null cannot be converted to Int")
-      case i: Int => i
-      case l: Long => l.toInt
-      case f: Float => f.toInt
-      case d: Double => d.toInt
-      case bd: BigDecimal => bd.toInt
-      case s: String => s.trim().toInt
-      case _ => throw new RuntimeException(s"${value.getClass.getName} toInt is not implemented")
-    }
-  }
 
   def toFloat(value: Any): Float = {
     value match {
@@ -92,20 +32,6 @@ object DataTypesConvertor {
       case bd: BigDecimal => bd.toFloat
       case s: String => s.trim().toFloat
       case _ => throw new RuntimeException(s"${value.getClass.getName} toFloat is not implemented")
-    }
-  }
-
-  def toStr(value: Any): String = {
-    value match {
-      case null => null
-      case s: String => s
-      case value =>
-        try {
-          value.toString
-        }
-        catch {
-          case e: Exception => throw new RuntimeException(s"${value.getClass.getName} toStr is not implemented")
-        }
     }
   }
 }
---
+++ spark-tf-core/src/main/scala/org/tensorflow/TensorflowRelation.scala
@@ -0,0 +1,32 @@
+package org.tensorflow
+
+import org.apache.hadoop.io.{ BytesWritable, NullWritable }
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.{ Row, SQLContext, SparkSession }
+import org.apache.spark.sql.sources.{ BaseRelation, TableScan }
+import org.apache.spark.sql.types.StructType
+import org.tensorflow.example.Example
+import org.tensorflow.hadoop.io.TFRecordFileInputFormat
+import org.tensorflow.serde.DefaultTfRecordRowDecoder
+
+case class TensorflowRelation(options: Map[String, String])(@transient val session: SparkSession) extends BaseRelation with TableScan {
+
+  //Import TFRecords as DataFrame happens here
+  lazy val (tf_rdd, tf_schema) = {
+    val rdd = session.sparkContext.newAPIHadoopFile(options("path"), classOf[TFRecordFileInputFormat], classOf[BytesWritable], classOf[NullWritable])
+
+    val exampleRdd = rdd.map {
+      case (bytesWritable, nullWritable) => Example.parseFrom(bytesWritable.getBytes)
+    }
+
+    val finalSchema = TensorflowInferSchema(exampleRdd)
+
+    (exampleRdd.map(example => DefaultTfRecordRowDecoder.decodeTfRecord(example, finalSchema)), finalSchema)
+  }
+
+  override def sqlContext: SQLContext = session.sqlContext
+
+  override def schema: StructType = tf_schema
+
+  override def buildScan(): RDD[Row] = tf_rdd
+}
